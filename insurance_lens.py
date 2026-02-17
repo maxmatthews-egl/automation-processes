5420847e467f60e7f2368d79b2b10f1bf0f8c1d5
Max Matthews <matthewsm@enstargroup.com>
2026-02-17 11:27:13 +0000

    Version with combined 3xN dataframe working

--- insurance_lens.py
+++ insurance_lens.py
@@ -8,27 +8,30 @@ import pandas as pd
 df = pd.read_csv(r"C:\\Users\\matthewsm\\OneDrive - Enstargroup\\2026 Work\\02. Feb\\three_lens_tst\\One-Year Reserve Risk by Class.csv")
 df = df.dropna(how = 'all', axis = 1) #Drop all N/A cols
 
+#Combining QBE2 into one class
+df['qbe2'] = df['qbe_atom23_lloyds'] + df['qbe_atom23_other']
+df.insert(1, 'qbe2', df.pop('qbe2'))
+df = df.drop(columns=['qbe_atom23_lloyds', 'qbe_atom23_other'])
+df['total'] = df.iloc[:,1:].sum(axis = 1) # Creating total col excl. sim number
 
+#Taking Gross & RI Reserves to calculate Net Reserves
 reserves = pd.read_excel(r"C:\\Users\\matthewsm\\OneDrive - Enstargroup\\2026 Work\\02. Feb\\three_lens_tst\\net_reserves.xlsx")
 reserves.columns = ["class", "reserve_type", "year", "amount"]
 reserves = reserves.pivot_table(index="class", columns="reserve_type", values="amount", aggfunc="sum").fillna(0)
 reserves['net'] = reserves['Gross Reserves'] + reserves['RI Reserves']
 
+
+#Taking just net reserves
 net_res = reserves['net'].to_frame().T 
-net_res = net_res.rename(columns = {'Total Insurance': 'total'})
+net_res = net_res.rename(columns = {'Total Insurance': 'total'}) # Rename of column to align with df
+#Combining QBE2 into one class
 net_res['qbe2'] = net_res['qbe_atom23_lloyds'] + net_res['qbe_atom23_other']
 net_res.insert(1, 'qbe2', net_res.pop('qbe2'))
 net_res = net_res.drop(columns=['qbe_atom23_lloyds', 'qbe_atom23_other'])
-
-
-
+net_res = net_res.reindex(columns=df.columns, fill_value=0)
 
 '''
-#Section 1: Finding Net Reserves
-'''
-
-'''
-#Section 1: Finding 95th Percentile Losses
+Section 2: Finding 95th Percentile Losses
 '''
 no_sims = len(df)
 sims_selected = 101
@@ -38,16 +41,7 @@ sims_selected = 101
 start = 9949
 end = 10050
 
-#Combining QBE2 into one class
-df['qbe2'] = df['qbe_atom23_lloyds'] + df['qbe_atom23_other']
-df.insert(1, 'qbe2', df.pop('qbe2'))
-df = df.drop(columns=['qbe_atom23_lloyds', 'qbe_atom23_other'])
 
-net_res = net_res.reindex(columns=df.columns, fill_value=0)
-print(net_res)
-
-
-df['total'] = df.iloc[:,1:].sum(axis = 1) # Creating total col excl. sim number
 df = df.sort_values('total') #Sorting
 
 #Creating Undiversified 1:20
@@ -56,7 +50,11 @@ undiversified = df.iloc[:,1:].quantile(0.05)
 diversified = df.iloc[start:end, 1:].mean(axis = 0, skipna = True)
 #Combining into single DF
 var_95 = pd.concat([undiversified, diversified], axis = 1).T
+var_95 = var_95.rename(index={0.05: 'undiversified', 0.00: 'diversified'})
 
-print(var_95)
 
-#var_95.to_excel('test_df.xlsx')
+'''
+Section 3: Combining dataframes
+'''
+ouptut_df = pd.concat([net_res,var_95], axis = 0, join = 'inner') # Inner join takes intersection and drops cols not present in both ('Sim' in this case)
+print(ouptut_df)
